6464be42f5de55e247152cd1b617fc218de675a4
jmisciagno <johnmisciagno@gmail.com>
2025-04-10 22:34:45 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+minikanren-with-forall
+==========
+
+Copyright (C) 2025 John Misciagno
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+Example:
+
+A "forall" expression succeeds if and only its newly introduced variables are unbound.
+
+An "exists" expression is equivalent to a "fresh" expression.
+
+```
+(run* (q) (forall (p) (== p p))) ; => succeeds
+(run* (q) (forall (p) (membero p (list p)))) ; => succeeds
+(run* (q) (forall (p) (exists (r) (== p r)))) ; => succeeds
+(run* (q) (forall (p) (== p 0))) ; => fails
+(run* (q) (forall (p p0) (== p p0))) ; => fails
+(run* (q) (exists (p) (forall (p0) (== p p0)))) ; => fails
+
+(run* (q) (exists (p) (forall (p0) (conde ((== p p0) (== q 99))
+				   ((== p p)  (== q 100)))))) ; => (100)
+
+(run* (q) (forall (p) (exists (p0) (conde ((== p p0) (== q 99))
+			      	   	  ((== p p)  (== q 100)))))) ; => (99 100)
+
+```
---
+++ mk.scm
@@ -0,0 +1,177 @@
+;; Jason Hemann and Dan Friedman
+;; microKanren, final implementation from paper
+;; Edited by John Misciagno in 2025
+
+(define (var x) (vector x))
+(define (var? x) (vector? x))
+(define (var=? x1 x2) (and (var? x1) (var? x2) (= (vector-ref x1 0) (vector-ref x2 0))))
+
+(define (var<? x1 x2) (< (vector-ref x1 0) (vector-ref x2 0)))
+(define (var-u x) (vector x 'U)) ; tag var as universally quantified
+(define (var-u? x) (and (vector? x) (> (vector-length x) 1) (eq? (vector-ref x 1) 'U)))
+
+(define (walk u s)
+  (let ((pr (and (var? u) (assp (lambda (v) (var=? u v)) s))))
+    (if pr (walk (cdr pr) s) u)))
+  
+(define occurs-check
+  (lambda (x v s)
+    (let ((v (walk v s)))
+      (cond
+        ((var? v) (eq? v x))
+        ((pair? v)
+         (or
+           (occurs-check x (car v) s)
+           (occurs-check x (cdr v) s)))
+        (else #f)))))
+
+(define ext-s-check
+  (lambda (x v s)
+    (cond
+      ((occurs-check x v s) #f)
+      (else (cons `(,x . ,v) s)))))
+
+(define (== u v)
+  (lambda (s/c)
+    (let ((s (unify u v (car s/c))))
+      (if s (unit `(,s . ,(cdr s/c))) mzero))))
+
+(define (unit s/c) (cons s/c mzero))
+(define mzero '())
+
+; should fail if first var is var-u? and 
+; second var is either not a var?, is a var-u?, var<? first var
+(define (bound-to u v) (or (not (var? u)) (var-u? u) (var<? u v)))
+
+(define (unify u v s)
+  (let ((u (walk u s)) (v (walk v s)))
+    (cond
+     ((and (var? u) (var? v) (var=? u v)) s)
+     ((and (var-u? u) (or (not (var? v)) (var-u? v) (var<? v u))) #f)
+     ((and (var-u? v) (or (not (var? u)) (var-u? u) (var<? u v))) #f)
+     ((var? u) (ext-s-check u v s))
+     ((var? v) (ext-s-check v u s))
+     ((and (pair? u) (pair? v))
+      (let ((s (unify (car u) (car v) s)))
+        (and s (unify (cdr u) (cdr v) s))))
+     (else (and (eqv? u v) s)))))
+
+(define (call/fresh f)
+  (lambda (s/c)
+    (let ((c (cdr s/c)))
+      ((f (var c)) `(,(car s/c) . ,(+ c 1))))))
+
+(define (call/fresh-u f)
+  (lambda (s/c)
+    (let ((c (cdr s/c)))
+      ((f (var-u c)) `(,(car s/c) . ,(+ c 1))))))
+
+(define (disj g1 g2) (lambda (s/c) (mplus (g1 s/c) (g2 s/c))))
+(define (conj g1 g2) (lambda (s/c) (bind (g1 s/c) g2)))
+
+(define (mplus $1 $2)
+  (cond
+    ((null? $1) $2)
+    ((procedure? $1) (lambda () (mplus $2 ($1))))
+    (else (cons (car $1) (mplus (cdr $1) $2)))))
+
+(define (bind $ g)
+  (cond
+    ((null? $) mzero)
+    ((procedure? $) (lambda () (bind ($) g)))
+    (else (mplus (g (car $)) (bind (cdr $) g)))))
+
+;;;; How to make a simple miniKanren (substitution only)
+
+(define-syntax Zzz
+  (syntax-rules ()
+    ((_ g) (lambda (s/c) (lambda () (g s/c))))))
+
+(define-syntax conj+
+  (syntax-rules ()
+    ((_ g) (Zzz g))
+    ((_ g0 g ...) (conj (Zzz g0) (conj+ g ...)))))
+
+(define-syntax disj+
+  (syntax-rules ()
+    ((_ g) (Zzz g))
+    ((_ g0 g ...) (disj (Zzz g0) (disj+ g ...)))))
+
+(define-syntax fresh
+  (syntax-rules ()
+    ((_ () g0 g ...) (conj+ g0 g ...))
+    ((_ (x0 x ...) g0 g ...)
+     (call/fresh
+      (lambda (x0)
+        (fresh (x ...) g0 g ...))))))
+
+(define-syntax forall
+  (syntax-rules ()
+    ((_ () g0 g ...) (conj+ g0 g ...))
+    ((_ (x0 x ...) g0 g ...)
+     (call/fresh-u
+      (lambda (x0)
+        (forall (x ...) g0 g ...))))))
+
+(define-syntax exists
+  (syntax-rules ()
+    ((exists (v ...) g0 g ...)
+     (fresh (v ...) g0 g ...))))
+
+(define-syntax conde
+  (syntax-rules ()
+    ((_ (g0 g ...) ...) (disj+ (conj+ g0 g ...) ...))))
+
+(define-syntax run
+  (syntax-rules ()
+    ((_ n (x ...) g0 g ...)
+     (map reify-1st (take n (call/goal (fresh (x ...) g0 g ...)))))))
+
+(define-syntax run*
+  (syntax-rules ()
+    ((_ (x ...) g0 g ...)
+     (map reify-1st (take-all (call/goal (fresh (x ...) g0 g ...)))))))
+
+(define empty-state '(() . 0))
+
+(define (call/goal g) (g empty-state))
+
+(define (pull $)
+  (if (procedure? $) (pull ($)) $))
+
+(define (take-all $)
+  (let (($ (pull $)))
+    (if (null? $) '() (cons (car $) (take-all (cdr $))))))
+
+(define (take n $)
+  (if (zero? n) '()
+    (let (($ (pull $)))
+      (if (null? $) '() (cons (car $) (take (- n 1) (cdr $)))))))
+
+(define (reify-1st s/c)
+  (let ((v (walk* (var 0) (car s/c))))
+    (walk* v (reify-s v '()))))
+
+(define (walk* v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v) v)
+      ((pair? v) (cons (walk* (car v) s)
+                   (walk* (cdr v) s)))
+      (else  v))))
+
+(define (reify-s v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v)
+       (let  ((n (reify-name (length s))))
+         (cons `(,v . ,n) s)))
+      ((pair? v) (reify-s (cdr v) (reify-s (car v) s)))
+      (else s))))
+
+(define (reify-name n)
+  (string->symbol
+    (string-append "_" "." (number->string n))))
+
+(define succeed (== #f #f))
+(define fail (== #f #t))
