f68af57b88e15002268405759eaaf16d00fef1d0
jmisciagno <johnmisciagno@gmail.com>
2025-04-18 11:53:38 -0400

    Add files via upload

--- mk.scm
+++ mk.scm
@@ -13,7 +13,15 @@
 (define (walk u s)
   (let ((pr (and (var? u) (assp (lambda (v) (var=? u v)) s))))
     (if pr (walk (cdr pr) s) u)))
-  
+
+(define (walk-pair x s)
+  (let ((res (walk x s)))
+      (cons x res)))
+
+(define (vars g)
+  (lambda (s/c)
+    (map car (take-all (g s/c)))))
+
 (define occurs-check
   (lambda (x v s)
     (let ((v (walk v s)))
@@ -39,17 +47,39 @@
 (define (unit s/c) (cons s/c mzero))
 (define mzero '())
 
-; should fail if first var is var-u? and 
-; second var is either not a var?, is a var-u?, var<? first var
-(define (bound-to u v) (or (not (var? u)) (var-u? u) (var<? u v)))
+(define (_and a b) (and a b))
+(define (_or a b) (or a b))
 
+(define (fact-equal? f0 f1)
+  (and (equal? (car f0) (car f1))
+       (or (and (equal? (cdr f0) (cdr f1)))
+	   (and (var? (cdr f0)) (not (var-u? (cdr f0))))
+	   (and (var? (cdr f1)) (not (var-u? (cdr f1)))))))
+
+(define (flip p) (cons (cdr p) (car p)))
+
+(define (fact-member f fs)
+  (or (equal? fs (unify (car f) (cdr f) fs)) ; check for added information
+      (fold-left _or
+		 #f
+		 (map (lambda (f0)
+			(or (fact-equal? f f0)
+			    (fact-equal? f (flip f0))
+			    (fact-equal? (flip f) f0)
+			    (fact-equal? (flip f) (flip f0))))
+		      fs))))
+
+(define (fact-in-worlds? f ws)
+  (fold-left _or
+	     #f
+	     (map (lambda (w) (fact-member f (car w))) ws)))
+  
 (define (unify u v s)
   (let ((u (walk u s)) (v (walk v s)))
     (cond
      ((and (var? u) (var? v) (var=? u v)) s)
-     ((and (var-u? u) (or (not (var? v)) (var-u? v) (var<? v u))) #f)
-     ((and (var-u? v) (or (not (var? u)) (var-u? u) (var<? u v))) #f)
      ((var? u) (ext-s-check u v s))
+     ((and (var? u) (var? v) (var<? v u)) (ext-s-check v u s)) ; normalize var order
      ((var? v) (ext-s-check v u s))
      ((and (pair? u) (pair? v))
       (let ((s (unify (car u) (car v) s)))
@@ -66,8 +96,83 @@
     (let ((c (cdr s/c)))
       ((f (var-u c)) `(,(car s/c) . ,(+ c 1))))))
 
-(define (disj g1 g2) (lambda (s/c) (mplus (g1 s/c) (g2 s/c))))
-(define (conj g1 g2) (lambda (s/c) (bind (g1 s/c) g2)))
+; should fail if first var is var-u? and 
+; second var is either not a var?, is a var-u?, var<? first var
+(define (bound-to u v) (or (not (var? u))
+			   (and (var-u? u) (not (var=? u v)))
+			   (var<? u v)))
+
+
+(define (call/forall f)
+  (lambda (s/c)
+    (let ((c (cdr s/c)))
+	(let ((res (take-all ((call/fresh-u f) s/c))))
+	  (filter (lambda (s)
+		    (not (or (cond ((walk-pair (var-u c) (car s)) =>
+				    (lambda (p)
+				      (bound-to (cdr p) (car p))))
+				   (else #f))
+			     (cond ((walk-pair (var-u c) (map flip (car s))) =>
+				    (lambda (p)
+				      (bound-to (cdr p) (car p))))
+				   (else #f)))))
+		  res)))))
+
+(define (_conj g1 g2)
+  (lambda (s/c)
+    (bind (g1 s/c) g2)))
+
+; disj with unique variables accross possible worlds
+(define (_disj g1 g2) 
+  (lambda (s/c)
+    (let* ((g1-s/c (g1 s/c))
+	   (c (get-c g1-s/c)))
+      (mplus g1-s/c (g2 (+c s/c c))))))
+
+(define (get-c x)
+  (cond ((null? x) 0)
+	((procedure? x) (get-c (x)))
+	((pair? x) (+ (cdr (car x)) (get-c (cdr x))))
+	(else x)))
+
+(define (+c s/c n)
+  (cons (car s/c) (+ (cdr s/c) n)))
+
+(define (remove-universal-aux2 s)
+  (filter (lambda (p)
+	    (and (not (var-u? (car p))) (not (var-u? (cdr p)))))
+	  s))
+  
+(define (remove-universal-aux s)
+  (map (lambda (w)
+	 (cons (remove-universal-aux2 (car w)) (cdr w)))
+       s))
+
+(define (remove-universal g)
+  (lambda (s/c)
+    (remove-universal-aux (take-all (g s/c)))))
+
+
+(define (implies g1 g2)
+  (lambda (s/c)
+    (let ((g1-res (take-all (g1 s/c))))
+      (let ((g2-res (take-all (g2 s/c))))
+      ((fold-left _conj
+		  succeed
+		  (map (lambda (x)
+			 (let ((t (take-all (bind (unit x) g2))))
+			   (if (null? t)
+			       fail
+			       (if (fold-left _and #t
+					      (fold-left _or
+							 #f
+							 (map (lambda (x0) (map (lambda (x1) (fact-in-worlds? x1 g1-res)) (car x0))) g2-res)))
+				     (remove-universal (_conj g1 g2))
+				     fail))))
+		       g1-res)) s/c)))))
+
+
+
 
 (define (mplus $1 $2)
   (cond
@@ -87,19 +192,19 @@
   (syntax-rules ()
     ((_ g) (lambda (s/c) (lambda () (g s/c))))))
 
-(define-syntax conj+
+(define-syntax conj
   (syntax-rules ()
     ((_ g) (Zzz g))
-    ((_ g0 g ...) (conj (Zzz g0) (conj+ g ...)))))
+    ((_ g0 g ...) (_conj (Zzz g0) (conj g ...)))))
 
-(define-syntax disj+
+(define-syntax disj
   (syntax-rules ()
     ((_ g) (Zzz g))
-    ((_ g0 g ...) (disj (Zzz g0) (disj+ g ...)))))
+    ((_ g0 g ...) (_disj (Zzz g0) (disj g ...)))))
 
 (define-syntax fresh
   (syntax-rules ()
-    ((_ () g0 g ...) (conj+ g0 g ...))
+    ((_ () g0 g ...) (conj g0 g ...))
     ((_ (x0 x ...) g0 g ...)
      (call/fresh
       (lambda (x0)
@@ -107,12 +212,12 @@
 
 (define-syntax forall
   (syntax-rules ()
-    ((_ () g0 g ...) (conj+ g0 g ...))
+    ((_ () g0 g ...) (conj g0 g ...))
     ((_ (x0 x ...) g0 g ...)
-     (call/fresh-u
+     (call/forall
       (lambda (x0)
         (forall (x ...) g0 g ...))))))
-
+	  
 (define-syntax exists
   (syntax-rules ()
     ((exists (v ...) g0 g ...)
@@ -120,7 +225,7 @@
 
 (define-syntax conde
   (syntax-rules ()
-    ((_ (g0 g ...) ...) (disj+ (conj+ g0 g ...) ...))))
+    ((_ (g0 g ...) ...) (disj (conj g0 g ...) ...))))
 
 (define-syntax run
   (syntax-rules ()
